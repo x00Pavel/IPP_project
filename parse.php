dcc718419bac462d7e6a441a78aa3f38084fea1e
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-02-14 20:50:57 +0100

    add check for valid commands, add default info to XML file

--- parse.php
+++ parse.php
@@ -1,6 +1,33 @@
 <?php
 
-include './functions.php';
+
+/*
+21 - chybná nebo chybějící hlavička ve zdrojovém kódu zapsaném v IPPcode20;
+22 - neznámý nebo chybný operační kód ve zdrojovém kódu zapsaném v IPPcode20;
+23 - jiná lexikální nebo syntaktická chyba zdrojového kódu zapsaného v IPPcode20.
+*/
+include 'functions.php';
+
+$commands = array(
+    "MOVE" => 2, 
+    "CREATEFRAME" => 0, "PUSHFRAME" => 0, "POPFRAME" => 0, 
+    "DEFVAR" => 1,
+    "CALL" => 1, 
+    "RETURN" => 0, 
+    "PUSHS" => 1, "POPS" => 1, 
+    "ADD" => 3,"SUB" => 3, "MUL" => 3, "IDIV" => 3, 
+    "LT" => 3, "GT" => 3, "EQ" => 3, 
+    "AND" => 3, "OR" => 3, "NOT" => 3, 
+    "INT2CHAR" => 2, "STRI2INT" => 3, 
+    "READ"  => 2, "WRITE" => 1, 
+    "CONCAT" => 3, "STRLEN" => 2, "GETCHAR" => 3,"SETCHAR" => 3, 
+    "TYPE" => 2, "LABEL" => 1, 
+    "JUMP" => 1, "JUMPIFEQ" => 3, "JUMPIFNEQ" => 3, 
+    "EXIT" => 1, 
+    "DPRINT" => 1, "BREAK" => 0); 
+
+
+$beginning = true;
 
 // Handling about number of arguments and printing out help massage
 if ($argc >= 2){
@@ -22,6 +49,16 @@ if ($argc >= 2){
     }    
 }
 
+// Create XML instance and write down default info
+$xw = xmlwriter_open_memory();
+xmlwriter_set_indent($xw, true);
+$res = xmlwriter_set_indent_string($xw, '  ');
+xmlwriter_start_document($xw, '1.0', 'UTF-8');
+xmlwriter_start_element($xw, 'program');
+xmlwriter_start_attribute($xw, 'language');
+xmlwriter_text($xw, 'IPPcode20');
+xmlwriter_end_attribute($xw);
+
 // Reading code from standard input
 while ($input_code = fgets(STDIN)){
     switch ($input_code[0]){
@@ -29,10 +66,36 @@ while ($input_code = fgets(STDIN)){
             continue;
         case "\n":
             continue;
+        case ".": // Initial string
+            $rc = checkHeader($input_code, $beginning);
+            if($rc != 0){
+                exit ($rc);
+            }
+            
+            continue;
         default:
-            echo $input_code;
+            $rc = checkArgsCount($input_code);
+            if($rc != 0){
+                exit ($rc);
+            }
+            // Check if it is valid operation code
+            if (!array_key_exists(strtoupper($input_code[0]), $commands)){
+                fwrite(STDERR, "Wrong command '".$input_code[0]."'\n");
+                xmlwriter_end_document($xw);
+                exit (22);
+            }
+            else{
+                // TODO 
+                // write down arguments to xml file (read specifications)
+            }
+            
+            continue;
 
+        
     }
 } 
+xmlwriter_end_element($xw);
+xmlwriter_end_document($xw);
 
+echo xmlwriter_output_memory($xw);
 ?>
